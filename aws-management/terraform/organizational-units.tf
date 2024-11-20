6469c271de3f148999a8a50fcbe74a0538b0ce89
Jana Hockenberger <jana.hockenberger@pcg.io>
2024-11-20 14:18:25 +0100

    Added more OUs

--- aws-management/terraform/organizational-units.tf
+++ aws-management/terraform/organizational-units.tf
@@ -15,6 +15,24 @@ resource "aws_organizations_organizational_unit" "sdlc" {
   parent_id = var.root_ou
 }
 
+
+resource "aws_organizations_organizational_unit" "germany" {
+  name      = "Germany"
+  parent_id = aws_organizations_organizational_unit.qualified_workloads.id
+}
+
+
+resource "aws_organizations_organizational_unit" "usa" {
+  name      = "USA"
+  parent_id = aws_organizations_organizational_unit.qualified_workloads.id
+}
+
+resource "aws_organizations_organizational_unit" "china" {
+  name      = "China"
+  parent_id = aws_organizations_organizational_unit.qualified_workloads.id
+}
+
+
 variable "root_ou" {
   type        = string
   default     = "r-puch"
