31472d9e39933d3ff8b20153e0ec2b6bde7e56b3
Jana Hockenberger <jana.hockenberger@pcg.io>
2024-11-25 13:51:16 +0100

    added 2 s3 buckets with object lock enabled

---
+++ aws-backup-china/terraform/backup-s3.tf
@@ -0,0 +1,25 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "immutable_bucket" {
+  bucket = "backup-china-${data.aws_caller_identity.current.account_id}"
+}
+
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.immutable_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_object_lock_configuration" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  rule {
+    default_retention {
+      mode = "GOVERNANCE"
+      days = 30
+    }
+  }
+}
